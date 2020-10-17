0c639f3ac8095ff9bdd4347d73aeba615e99dcd6
rinsuki <428rinsuki+git@gmail.com>
2020-10-18 01:30:35 +0900

    export ParamsFromPath

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import { Params } from "./params"
+import { PathASTComputed } from "./parser"
+
 export { match } from "./match"
 export { parser } from "./parser"
+export type ParamsFromPath<Path extends string> = Params<PathASTComputed<Path>>
